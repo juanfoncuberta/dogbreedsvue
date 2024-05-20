0fbc573e86117a2fccfe05d9a0656b290d54f595
Juan Foncuberta <juan.foncuberta@gmail.com>
2024-05-20 15:51:22 +0200

    throwing errors when api returns error

--- src/composables/fetchApi.js
+++ src/composables/fetchApi.js
@@ -6,17 +6,15 @@ export const fetchApi = () => {
   const data = ref(null)
   const getData = async(url) => {
     try{
-      console.log('dentro getdata')
-      //const data = await axios.get(`https://dog.ceo/api/breed/`+useRoute().params.breed+`/images/random`)
+
        const result = await axios.get(url)
        data.value = result.data
        
-       return result.data
-      // if(result.status == 200)
-      //    // urlImage.value = data.data.message
-      //   return result.data.message
-      // else
-      //   throw new TypeError(data.statusText)
+   
+      if(result.status == 200)
+         return result.data
+      else
+        throw new TypeError(data.statusText)
     }catch(error){
       console.log("Error getting breed detail from api",error)
     }
