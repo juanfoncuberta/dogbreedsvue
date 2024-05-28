7a549cdcdc4a27f499b73708080bc15eb670766c
Juan Foncuberta <juan.foncuberta@gmail.com>
2024-05-28 16:23:01 +0200

    Saving data in localstorage

--- src/store/favs.js
+++ src/store/favs.js
@@ -1,9 +1,13 @@
 import { defineStore } from "pinia";
+
 import { ref, computed } from 'vue'
 
 export const useFavsStore = defineStore("favs",() => {
 
   const favs = ref([])
+
+  if(localStorage.getItem('favs')) 
+      favs.value = JSON.parse(localStorage.getItem('favs'))
 
   const toggleFav = (dogBreed) => {
 
@@ -12,7 +16,8 @@ export const useFavsStore = defineStore("favs",() => {
             favs.value.splice(index, 1); 
         else
             favs.value.push(dogBreed)
-
+      
+        localStorage.setItem('favs',JSON.stringify(favs.value))
   }
   
   return {
